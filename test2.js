87b2feb5fd420e4fd145b76495e968cb86c59f9b
zsoltbringye <zsolt.bringye@gmail.com>
2019-06-06 15:45:17 +0200

    Update test2.js

--- test2.js
+++ test2.js
@@ -2,6 +2,8 @@
 
 
 (function(ext) {
+    var alarm_went_off = false; // This becomes true after the alarm goes off
+
     // Cleanup function when the extension is unloaded
     ext._shutdown = function() {};
 
@@ -11,18 +13,31 @@
         return {status: 2, msg: 'Ready'};
     };
 
-    ext.power = function(base, exponent) {
-        return Math.pow(base, exponent);
+    ext.set_alarm = function(time) {
+       window.setTimeout(function() {
+           alarm_went_off = true;
+       }, time*1000);
+    };
+
+    ext.when_alarm = function() {
+       // Reset alarm_went_off if it is true, and return true
+       // otherwise, return false.
+       if (alarm_went_off === true) {
+           alarm_went_off = false;
+           return true;
+       }
+
+       return false;
     };
 
     // Block and block menu descriptions
     var descriptor = {
         blocks: [
-            // Block type, block name, function name, param1 default value, param2 default value
-            ['r', '%n ^ %n', 'power', 2, 3],
+            ['', 'run alarm after %n seconds', 'set_alarm', '2'],
+            ['h', 'when alarm goes off', 'when_alarm'],
         ]
     };
 
     // Register the extension
-    ScratchExtensions.register('Sample extension', descriptor, ext);
+    ScratchExtensions.register('Alarm extension', descriptor, ext);
 })({});
